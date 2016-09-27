3dfe6f0326e40a31c293d1e6452854601ac89724
umohnani <umohnani@bu.edu>
2016-09-26 22:42:54 -0400

    Implemented the database and successfully inserted temperature readings to the database. yayyy!!!!!

--- challenge2/challenge2.js
+++ challenge2/challenge2.js
@@ -9,16 +9,21 @@ var io = require('socket.io')(http);
 var url = 'mongodb://localhost:27017/test';
 
 // Connect to DB
-/*
-mongo.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log('Connected to mongodb server');
-  db.close();
-});
-*/
+
+var insertTemp = function(id, temp, db, callback) {
+	db.collection('temperatures').insertOne({
+		"XBee_id" : id,
+		"time" : new Date(),
+		"temp" : temp
+	}, function(err, result) {
+		assert.equal(err, null);
+		console.log("Inserted a temperature!!!!!!!!");
+		callback();
+	});
+};
 
 // Connect to serialport
-/*
+
 var portName = process.argv[2],
 portConfig = {
     baudRate: 9600,
@@ -26,7 +31,25 @@ portConfig = {
 };
 var sp;
 sp = new serialport.SerialPort(portName, portConfig);
-*/
+
+sp.on("open", function() {
+	console.log("open");
+	sp.on("data", function(data) {
+		var id = parseInt(data.split(":")[0]);
+    	var temp = parseInt(data.split(":")[1]);
+
+    	//Insert into the database called temperatures
+    	//To print the database, enter "mongo". Then "db.temperatures.find().pretty()" in the terminal. 
+    	mongo.connect(url, function(err, db) {
+		  assert.equal(null, err);
+		  console.log('Connected to mongodb server');
+		  insertTemp(id, temp, db, function() {
+		  	db.close();
+		  });
+		});
+	});
+});
+
 
 // Return the html page and other web things
 app.use(express.static('public'));
