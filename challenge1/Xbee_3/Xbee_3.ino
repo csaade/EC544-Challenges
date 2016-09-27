2ee5a4f3c0cae51180ab2d233f2571be8b2b109e
umohnani <umohnani@bu.edu>
2016-09-26 21:31:40 -0400

    xbee 3 code

--- challenge1/Xbee_3/Xbee_3.ino
+++ challenge1/Xbee_3/Xbee_3.ino
@@ -21,7 +21,7 @@ SoftwareSerial XBee(2, 3); // RX, TX
 double Thermistor(int RawADC) {
  double Temp;
  Temp = log(10000.0*((1024.0/RawADC-1))); 
- Temp = 1 / (0.001129148 + (0.000231995 + (0.0000000876741 * Temp * Temp ))* Temp );
+ Temp = 1 / (0.001129148 + (0.000221995 + (0.0000000876741 * Temp * Temp ))* Temp );
  Temp = Temp - 273.15;           
  return Temp;
 }
