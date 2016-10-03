28afa7a8985a4d4879cc8b264cda79ccff551bda
umohnani <umohnani@bu.edu>
2016-10-03 19:53:04 -0400

    Average is working!!!!

--- challenge2/challenge2.js
+++ challenge2/challenge2.js
@@ -25,88 +25,6 @@ var insertTemp = function(id, temp, db, callback) {
 	});
 };
 
-/*
-** Function that sends all the data from the database to the front-end HTML
-** (1) Connects to database
-** (2) Submits a query to get the last data from the database (from all collections)
-** (3) Emits it (i.e. sends it to front-end)
-** NOTE: sends it to front end in the following format (format can be modified below)
-**		- XbeeID:TemperatureValue:TimeStamp
-*/
-var realtimeGraph = function(callback){
-
-	device_id = [];
-	graph_msg = [];
-
-	/*** CONNECTION TO DB ***/
-	// mongo.connect(url, function(err, db) {
-	// 	assert.equal(null, err);
-	// 	console.log('Connected to mongodb server'); //debug (remove this later)
-		
-	// 	/*** LOOPING THROUGH ALL COLLECTIONS INSIDE DB ***/
-	// 	//Note: every data coming from an Xbee has its own collection in the DB
-	// 	db.collections(function(e, cols) {
-	// 		cols.forEach(function(col) {
-
-	// 			var name = col.collectionName; //get XbeeID
-
-	// 			** FINDS LAST DATA INSERTED FROM SPECIFIC COLLECTION **
-	// 			/*** LOOPS THROUGH ALL DOCUMENTS INSIDE THIS DATAPOINT ***/
-	// 			db.collection(name).find().skip(db.collection(name).count() - 1).forEach(function(docs) {
-
-	// 				io.emit("DB Value", name + ":" + docs.temp + ":" + docs.time); //MODIFY THIS TO SEND IT IN DIFFERENT FORMAT
-	// 				console.log("id: " + name + " temp: " + docs.temp + " time: " + docs.time); //debug (can be removed)
-	// 			});
-	// 		});
-			
-	// 	//io.emit(graph_msg);
-	// 	});
-
-		 
-	// });
-
-	
-}
-
-// var average = function(callback) {
-// 	var total = 0;
-// 	var length = 0;
-// 	mongo.connect(url, function(err, db) {
-// 		assert.equal(null, err);
-// 		console.log('Connected to mongodb server'); //debug (remove this later)
-
-// 		/*** LOOPING THROUGH ALL COLLECTIONS INSIDE DB ***/
-// 		//Note: every data coming from an Xbee has its own collection in the DB
-// 		db.collections(function(e, cols) {
-// 			cols.forEach(function(col) {
-
-// 				var name = col.collectionName; //get XbeeID
-
-// 				/** FINDS LAST DATA INSERTED FROM SPECIFIC COLLECTION **/
-// 				/*** LOOPS THROUGH ALL DOCUMENTS INSIDE THIS DATAPOINT ***/
-// 				var docs = db.collection(name).find().sort({_id:-1}).limit(1);
-// 				//console.log(docs);
-// 					//  total = total + parseInt(docs.temp);
-// 					//  console.log("total: " + total);
-// 					//  //length += 1; 
-// 					//  console.log("length: " + length);
-// 					// // //console.log("average!!!!!!!!");
-// 					// console.log("temp: " + docs.temp);
-// 					// io.emit("DB Value", name + ":" + docs.temp + ":" + docs.time); //MODIFY THIS TO SEND IT IN DIFFERENT FORMAT
-// 			});
-
-
-// 		});	
-		
-// 	});
-// 	var average = parseInt(total/length);
-// 	total = 0;
-// 	//length = 0;
-// 	console.log("Average: " + average);
-// 	io.emit('Average', average);
-// }
-
-
 /***
 Function that sends all the data from the database to the front-end HTML
 ** (1) Connects to database
@@ -204,7 +122,7 @@ app.get('/historical/:xbeeId/timebackward/:time', function(req, res) {
 
 			res.setHeader('Content-Type', 'application/json');
 			res.send(JSON.stringify(xbeeData));
-		})
+		});
 
 		//Note: every data coming from an Xbee has its own collection in the DB
 		// db.collections(function(e, cols) {
