116c7559e6ba0e5680c3f045063aaba2441b21cc
Peter Goulakos <goulakos@bu.edu>
2016-10-11 18:37:11 -0400

    node script for ch 3

--- challenge3/challenge3.js
+++ challenge3/challenge3.js
@@ -19,13 +19,18 @@ sp = new serialport(portName, portConfig);
 sp.on("open", function() {
 	console.log("open");
 	sp.on("data", function(data) {
-		var id = data.split(":")[0];
-    	
-
-    	io.emit("DB Value", "X" + id + ":" + temp + ":" + time);
+        // Just received a status string from the arduino, sent it to the web page
+        io.emit('status_msg', data);
 	});
+});
 
-
+// When a socket connection occurs from the web page
+io.on('connection', function(socket) {
+    socket.on('led_msg', function(msg) {
+        // Received a message to toggle an LED from the web page,
+        // now send the character to the xbee->arduino
+        sp.write(msg);
+    });
 });
 
 // Return the html page and other web things
