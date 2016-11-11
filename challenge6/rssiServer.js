d7713415c0b0c368a0d10928001902d0fc10882a
Peter Goulakos <goulakos@bu.edu>
2016-11-11 14:36:17 -0500

    latest code

--- challenge6/rssiServer.js
+++ challenge6/rssiServer.js
@@ -3,54 +3,60 @@ var app = require('express')();
 var xbee_api = require('xbee-api');
 var fs = require('fs');
 var http = require('http').Server(app);
+var express = require('express');
 var io = require('socket.io')(http);
-
+var ml = require('machine_learning');
 
 var RSSI_values = [];
 
 /****** TCP/IP SERVER TO MATLAB *****/
-var net = require('net');
-net.createServer(connectListener).listen(8080);
+// var net = require('net');
+// net.createServer(connectListener).listen(8080);
 /***** SERVER ENDS HERE ******/
 
 
 /*** SERVER CONNECTION LISTENER *****/
-function connectListener(socket) {
-  socket.name = socket.remoteAddress+ ":" + socket.remotePort;
+// function connectListener(socket) {
+//   socket.name = socket.remoteAddress+ ":" + socket.remotePort;
 
-  //socket.write("shutup dude \n"); // this is how we write data to it 
-  //(eventually, we will be writing an array of RSS values)
-  if(RSSI_values.length == 4) {
-    var value_to_send = "[";
-    for(index in RSSI_values) {
-      value_to_send = value_to_send.concat(RSSI_values[index]);
-      console.log(index);
-      if(index != RSSI_values.length-1)
-        value_to_send = value_to_send.concat(",");
-    }
+//   //socket.write("shutup dude \n"); // this is how we write data to it 
+//   //(eventually, we will be writing an array of RSS values)
+//   if(RSSI_values.length == 4) {
+//     var value_to_send = "[";
+//     for(index in RSSI_values) {
+//       value_to_send = value_to_send.concat(RSSI_values[index]);
+//       console.log(index);
+//       if(index != RSSI_values.length-1)
+//         value_to_send = value_to_send.concat(",");
+//     }
 
-    value_to_send = value_to_send.concat("]");
-    console.log('sending to matlab: ' + value_to_send);
+//     value_to_send = value_to_send.concat("]");
+//     console.log('sending to matlab: ' + value_to_send);
 
-    socket.write(value_to_send);
+//     socket.write(value_to_send);
   
-    RSSI_values = [];
-    //Transmit all requests
-    for(address in xbeeAddress)
-        requestRSSI(xbeeAddress[address]);
-  }
-  // adding 'data' event handler on socket
-  socket.on('data', function(data) {
-    console.log("Matlab sent: " + data.toString());
-    io.emit('bin_number', data.toString());
-  });
+//     RSSI_values = [];
+//     //Transmit all requests
+//     for(address in xbeeAddress)
+//         requestRSSI(xbeeAddress[address]);
+//   }
+//   // adding 'data' event handler on socket
+//   socket.on('data', function(data) {
+//     console.log("Matlab sent: " + data.toString());
+//     io.emit('bin_number', data.toString());
+//   });
 
-  // closing connection
-  socket.on('close', function(data) {
-    console.log("closed " + data.toString());
-  });
+//   // closing connection
+//   socket.on('close', function(data) {
+//     console.log("closed " + data.toString());
+//   });
 
-}
+// }
+
+// Read output.csv into a 2d array
+var knn = null;
+var train_data = [];
+var bins = [];
 
 var C = xbee_api.constants;
 var XBeeAPI = new xbee_api.XBeeAPI({
@@ -89,23 +95,52 @@ function requestRSSI(address){
 
 }
 
+// Requests rssi's from all xbees
+function requestAllRSSI() {
+    for(address in xbeeAddress) {
+        requestRSSI(xbeeAddress[address]);
+    }
+}
+
 /* gets called whenever we connect to the server which is the serial port(only once) */
 sp.on("open", function () {
   console.log('open');
-  stream = fs.createWriteStream("./out.csv");
+  // stream = fs.createWriteStream("./out.csv");
 
   // generating the headers for csv
 
-  for(i=0; i<4; i++) {
-    stream.write("beacon"+(i+1).toString());
-    if(i != 3)
-      stream.write(",");
-  }
-  stream.write("\n");
+  // for(i=0; i<4; i++) {
+  //   stream.write("beacon"+(i+1).toString());
+  //   if(i != 3)
+  //     stream.write(",");
+  // }
+  // stream.write("\n");
 
   // send requests to all xbees
-  for(address in xbeeAddress)
-    requestRSSI(xbeeAddress[address]);
+  // requestAllRSSI();
+
+    console.log('Reading training data');
+    var file_data = fs.readFileSync('./output.csv');//, (err, data) => {
+    var lines = file_data.toString().split('\n');
+    for(var row = 1; row < lines.length - 1; row++) // skip first row and last row (newline)
+    {
+        var cols = lines[row].split(',');
+        // console.log(cols);
+        var row_array = [];
+        for(var col = 0; col < cols.length - 1; col++) // get the left 4 columns
+            row_array.push(parseInt(cols[col]));
+        
+        bins.push(cols[cols.length - 1]); // put the bin # in a different list
+        train_data.push(row_array);
+    }
+
+    knn = new ml.KNN({
+        data: train_data,
+        result: bins
+    });
+    console.log('Done');
+
+    requestAllRSSI();
 });
 
 /* Gets called whenever we receive data from xbee */
@@ -114,25 +149,38 @@ XBeeAPI.on("frame_object", function(frame) {
   if (frame.type == 144){
     //console.log('xbee' + frame.data[1].toString() + 'rssi' + frame.data[0].toString());
 
-    stream.write(frame.data[0].toString());
+    // stream.write(frame.data[0].toString());
     RSSI_values.push(frame.data[0].toString());
     //console.log(frame.data[0].toString());
     //countFrames++;
-
-
+    
     // if we read from all xbees
     if (RSSI_values.length == 4){
-      
-      stream.write("\n");
 
-      for(index in RSSI_values) {
-        console.log(RSSI_values[index]);
-      }      
+      // stream.write("\n");
+      var bin = knn.predict({
+        x: RSSI_values,
+        k: 3
+      });
+
+      console.log(RSSI_values.toString() + ' bin: ' + bin);
+      bin = parseInt(bin);
+      io.emit('msg', bin.toString());
+
+      console.log(RSSI_values)
+      RSSI_values = [];
+      requestAllRSSI();
     }
     // if we are still expecting data from xbees
-    else {
-      stream.write(",");
-    }
+    // else {
+    //   stream.write(",");
+    // }
   }
 
+});
+
+// Listen on port
+app.use(express.static('public'));
+http.listen(3000, function(){
+    console.log('listening on *:3000');
 });
