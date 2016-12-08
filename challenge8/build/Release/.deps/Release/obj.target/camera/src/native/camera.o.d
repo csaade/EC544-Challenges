0efb0b3074c97d1e082ee84d715ec8f111b0cb02
umohnani <umohnani@bu.edu>
2016-12-07 20:36:09 -0500

    live stream video

---
+++ challenge8/build/Release/.deps/Release/obj.target/camera/src/native/camera.o.d
@@ -0,0 +1,27 @@
+cmd_Release/obj.target/camera/src/native/camera.o := c++ '-DNODE_GYP_MODULE_NAME=camera' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/Users/Urvi/.node-gyp/4.5.0/include/node -I/Users/Urvi/.node-gyp/4.5.0/src -I/Users/Urvi/.node-gyp/4.5.0/deps/uv/include -I/Users/Urvi/.node-gyp/4.5.0/deps/v8/include -I/opt/local/include  -Os -gdwarf-2 -mmacosx-version-min=10.7 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++0x -fno-threadsafe-statics -fno-strict-aliasing -mmacosx-version-min=10.7 -std=c++11 -stdlib=libc++ -MMD -MF ./Release/.deps/Release/obj.target/camera/src/native/camera.o.d.raw   -c -o Release/obj.target/camera/src/native/camera.o ../src/native/camera.cpp
+Release/obj.target/camera/src/native/camera.o: ../src/native/camera.cpp \
+  /Users/Urvi/.node-gyp/4.5.0/include/node/node.h \
+  /Users/Urvi/.node-gyp/4.5.0/include/node/v8.h \
+  /Users/Urvi/.node-gyp/4.5.0/include/node/v8-version.h \
+  /Users/Urvi/.node-gyp/4.5.0/include/node/v8config.h \
+  /Users/Urvi/.node-gyp/4.5.0/include/node/node_version.h \
+  /Users/Urvi/.node-gyp/4.5.0/include/node/uv.h \
+  /Users/Urvi/.node-gyp/4.5.0/include/node/uv-errno.h \
+  /Users/Urvi/.node-gyp/4.5.0/include/node/uv-version.h \
+  /Users/Urvi/.node-gyp/4.5.0/include/node/uv-unix.h \
+  /Users/Urvi/.node-gyp/4.5.0/include/node/uv-threadpool.h \
+  /Users/Urvi/.node-gyp/4.5.0/include/node/uv-darwin.h \
+  /Users/Urvi/.node-gyp/4.5.0/include/node/pthread-barrier.h
+../src/native/camera.cpp:
+/Users/Urvi/.node-gyp/4.5.0/include/node/node.h:
+/Users/Urvi/.node-gyp/4.5.0/include/node/v8.h:
+/Users/Urvi/.node-gyp/4.5.0/include/node/v8-version.h:
+/Users/Urvi/.node-gyp/4.5.0/include/node/v8config.h:
+/Users/Urvi/.node-gyp/4.5.0/include/node/node_version.h:
+/Users/Urvi/.node-gyp/4.5.0/include/node/uv.h:
+/Users/Urvi/.node-gyp/4.5.0/include/node/uv-errno.h:
+/Users/Urvi/.node-gyp/4.5.0/include/node/uv-version.h:
+/Users/Urvi/.node-gyp/4.5.0/include/node/uv-unix.h:
+/Users/Urvi/.node-gyp/4.5.0/include/node/uv-threadpool.h:
+/Users/Urvi/.node-gyp/4.5.0/include/node/uv-darwin.h:
+/Users/Urvi/.node-gyp/4.5.0/include/node/pthread-barrier.h:
---
+++ challenge8/src/main.js
@@ -0,0 +1,127 @@
+var WebSocketServer = require("ws").Server;
+var cam = require("../build/Release/camera.node");
+var fs = require("fs");
+var websocketPort = 3000,
+    webPort = 9999,
+    openBrowser = true,
+    width = 640,
+    inputString = "",
+    height = 360;
+
+//Gathering Arguments
+process.argv.forEach(function (val, index, array) {
+    switch (val) {
+    case "-open":
+        openBrowser = true;
+        break;
+    case "-wsport":
+        websocketPort = parseInt(array[index + 1]);
+        break;
+    case "-webport":
+        webPort = parseInt(array[index + 1]);
+        break;
+    case "-res":
+        var res = array[index + 1].split("x");
+        width = parseInt(res[0]);
+        height = parseInt(res[1]);
+        break;
+    case "-input":
+        inputString = array[index + 1];
+        console.log(inputString);
+    }
+});
+
+var wss = new WebSocketServer({
+    port: websocketPort
+});
+
+var clients = {};
+
+var frameCallback = function (image) {
+    var frame = {
+        type: "frame",
+        frame: new Buffer(image, "ascii").toString("base64")
+    };
+    var raw = JSON.stringify(frame);
+    for (var index in clients) {
+        clients[index].send(raw);
+    }
+};
+
+var disconnectClient = function (index) {
+    delete clients[index];
+    if (Object.keys(clients).length == 0) {
+        console.log("No Clients, Closing Camera");
+        cam.Close();
+    }
+};
+
+var connectClient = function (ws) {
+    var index = "" + new Date().getTime();
+    console.log(cam.IsOpen());
+    if (!cam.IsOpen()) {
+        console.log("New Clients, Opening Camera");
+        cam.Open(frameCallback, {
+            width: width,
+            height: height,
+            window: false,
+            codec: ".jpg",
+            input: inputString
+        });
+    }
+    clients[index] = ws;
+    return index;
+};
+
+wss.on('connection', function (ws) {
+    var disconnected = false;
+    var index = connectClient(ws);
+
+    ws.on('close', function () {
+        disconnectClient(index);
+    });
+
+    ws.on('open', function () {
+        console.log("Opened");
+    });
+
+    ws.on('message', function (message) {
+
+        switch (message) {
+        case "close":
+            {
+                disconnectClient(index);
+            }
+            break;
+        case "size":
+            {
+                var size = cam.GetPreviewSize();
+
+                ws.send(JSON.stringify({
+                    type: "size",
+                    width: size.width,
+                    height: size.height
+                }));
+            }
+            break;
+        }
+    });
+
+});
+
+//Create Http Server
+var http = require("http");
+var index = fs.readFileSync(__dirname + "/../public/index2.html", 'utf8').replace("##webSocketPort", websocketPort);
+http.createServer(function (req, resp) {
+    resp.writeHead(200, {
+        "Content-Type": "text/html"
+    });
+    resp.end(index);
+}).listen(webPort);
+
+console.log("Http Server Started");
+
+if (openBrowser) {
+    var spawn = require('child_process').spawn
+    spawn('open', ['http://localhost:' + webPort]);
+}
