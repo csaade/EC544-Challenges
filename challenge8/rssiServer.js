023d3ef4399608d08e7b8ae7e0c698c7a76faf76
umohnani <umohnani@bu.edu>
2016-12-08 16:40:42 -0500

    video should be streaming live and website is basically set

--- challenge8/rssiServer.js
+++ challenge8/rssiServer.js
@@ -8,8 +8,8 @@ var io = require('socket.io')(http);
 var ml = require('machine_learning');
 var path = require('path');
 
-var spawn = require('child_process').spawn;
-var proc;
+// var spawn = require('child_process').spawn;
+// var proc;
  
 
 var RSSI_values = [];
@@ -244,45 +244,45 @@ io.on('connection', function(socket) {
     socket.on('remote_msg', function(msg) {
         // Received a message to toggle an LED from the web page,
         // now send the character to the xbee->arduino
-        //spArduino.write(msg);
+        spArduino.write(msg);
     });
 
-    socket.on('start-stream', function() {
-    startStreaming(io);
-  });
+  //   socket.on('start-stream', function() {
+  //   startStreaming(io);
+  // });
 
-    socket.on('disconnect', function() {
-      stopStreaming();
-    });
+  //   socket.on('disconnect', function() {
+  //     stopStreaming();
+  //   });
 
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
-});
+// http.listen(3000, function(){
+//     console.log('listening on *:3000');
+// });
 
-function stopStreaming() {
-  app.set('watchingFile', false);
-  if (proc) proc.kill();
-  fs.unwatchFile('./public/image_stream.jpg');
-}
+// function stopStreaming() {
+//   app.set('watchingFile', false);
+//   if (proc) proc.kill();
+//   fs.unwatchFile('./public/image_stream.jpg');
+// }
  
-function startStreaming(io) {
+// function startStreaming(io) {
  
-  if (app.get('watchingFile')) {
-    io.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
-    return;
-  }
+//   if (app.get('watchingFile')) {
+//     io.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
+//     return;
+//   }
  
-  var args = ["-w", "640", "-h", "480", "-o", "./public/image_stream.jpg", "-t", "999999999", "-tl", "1"];
-  proc = spawn('raspistill', args);
+//   var args = ["-w", "640", "-h", "480", "-o", "./public/image_stream.jpg", "-t", "999999999", "-tl", "1"];
+//   proc = spawn('raspistill', args);
  
-  console.log('Watching for changes...');
+//   console.log('Watching for changes...');
  
-  app.set('watchingFile', true);
+//   app.set('watchingFile', true);
  
-  fs.watchFile('./public/image_stream.jpg', function(current, previous) {
-    io.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
-  })
-}
+//   fs.watchFile('./public/image_stream.jpg', function(current, previous) {
+//     io.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
+//   })
+// }
 
